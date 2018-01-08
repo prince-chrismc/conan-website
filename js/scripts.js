eb0abeb949e67bafa789bbce261604ef3fd685eb
markusb <markusb@jfrog.com>
2018-01-08 11:53:24 +0200

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    //Activate user slider
+    $('.our-users-slider').slick({
+        variableWidth: true,
+        speed: 10000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        infinite: true,
+        cssEase: 'linear',
+        //gap: 0,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true
+    });
+
+    //Activate testimonials slider
+    $('.testimonial-slider').slick({
+        arrows: true,
+        autoplay: false,
+        dots: false,
+        speed: 800,
+        accessibility: true
+    });
+});
