f291f97c884d7260af587fcccd3a98211984d0b5
Nadav Cohen <nadavc@jfrog.com>
2020-03-12 16:19:48 +0200

    first path update

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,153 @@
-$(document).ready(function(){
-    //Activate user slider
-    $('.our-users-slider').slick({
-        variableWidth: true,
-        speed: 3000,
-        autoplay: true,
-        autoplaySpeed: 0,
-        infinite: true,
-        cssEase: 'linear',
-        //gap: 0,
-        arrows: false,
-        dots: false,
-        pauseOnHover: true
+// BOOTSTRAP VARS
+const bootstrap_sm = '769px';
+const bootstrap_md = '992px';
+
+
+jQuery(document).ready(function ($) {
+
+  //--ALL PAGES--//
+  //HAMBURGER
+  $('.hamburger').on('click',function () {
+    let ham = $(this);
+    ham.toggleClass('is-active');
+    toggleMenu(ham);
+  });
+
+  // $('[data-toggle="tooltip"]').tooltip();
+
+  //--HOME PAGE--//
+  if ( $('body.home').length ) {
+
+    //READ MORE SLIDE
+    $('.readMoreLink').on('click',function () {
+      let that = $(this);
+      let target = that.siblings('.readMoreContent');
+      $.when(that.fadeOut()).then(function () {
+        that.addClass('d-none');
+        target.slideDown();
+      });
     });
 
-    //Activate testimonials slider
-    $('.testimonial-slider').slick({
-        arrows: true,
-        autoplay: false,
-        dots: false,
-        speed: 800,
-        accessibility: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false
-                }
-            }
-        ]
+    // TESTIMONIALS CAROUSEL
+    $('.testis-carousel').slick({
+      prevArrow: $('.cs-slick-right'),
+      nextArrow: $('.cs-slick-left')
     });
 
-    configClipboard($);
-    configPopover($);
+    //MATCH HEIGHT, ADVANTAGES
+    if ( window.matchMedia("(min-width: "+bootstrap_sm+")").matches ) {
+      matchHeight($('.one-advantage-content .text'));
+    }
+
+  } //HOME PAGE
+
+  // DOWNLOADS
+  if ($('body.downloads').length) {
+    // TOOLTIPS
+
+    //cn-copy
+    $('.cn-copy')
+      .attr('data-original-title', "Copy pull command to clipboard")
+      .tooltip()
+      .click(function () {
+        let textToCopy = $(this).data('copy-value');
+        copyToClipboard(textToCopy);
+        $(this)
+        .tooltip('hide')
+        .attr('data-original-title', "Command Copied")
+        .tooltip('show');
+      });
+
+    //cn-options
+    $('.cn-options')
+      .tooltip()
+      .click(function () {
+        $(this)
+          .toggleClass('active');
+        if ($(this).hasClass('active')) {
+          $(this)
+            .tooltip('hide')
+            .attr('data-original-title', "Hide")
+            .tooltip('show');
+        } else {
+          $(this)
+            .tooltip('hide')
+            .attr('data-original-title', "Download")
+            .tooltip('show');
+        }
+
+      });
+
+    $('.cn-download').tooltip();
+
+  }
+
+
 });
 
-function configClipboard($) {
-    $('#downloads .artifactory-docker-image-cta').click(function (event) {
-        event.preventDefault();
-        var copytext = $(this).next('.to-copy').val();
-        console.log(copytext);
-        var $temp = $('<input>');
-        $('body').append($temp);
-        $temp.val(copytext).select();
-        var copied = document.execCommand('copy');
+jQuery(window).on('load', function () {
 
-        $temp.remove();
-        if (isSafariNine()) {
-            $(this).find('.popover-body').html('Press Command+C!');
-        }
-        else if (copied) {
-            $(this).find('.popover-body').html('Copied!');
-        }
-        else {
-            $(this).find('.popover-body').html('Error!');
-        }
-    });
+  //--DOWNLOADS--//
+  if ( $('body.downloads').length ) {
 
-    document.body.addEventListener('click', function () {
-    }, false);
-}
-
-function isSafariNine() {
-
-    navigator.sayswho = (function () {
-        var ua = navigator.userAgent, tem,
-            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-        if (/trident/i.test(M[1])) {
-            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-            return 'IE ' + (tem[1] || '');
-        }
-        if (M[1] === 'Chrome') {
-            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
-            if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-        }
-        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
-        return M.join(' ');
-    })();
-
-    if (navigator.sayswho.indexOf('Safari') > -1) {
-        var version = navigator.sayswho.replace('Safari ', '')
-        return parseInt(version) < 10;
-    } else {
-        return false;
+    //MATCH HEIGHT, ADVANTAGES
+    if ( window.matchMedia("(min-width: "+bootstrap_md+")").matches ) {
+      downloadsMatchHeight();
     }
+
+  }
+})
+
+//MENU TOGGLE
+function toggleMenu(ham) {
+
+  let hamOffset       = ham.offset();
+  let hamOffsetRight  = ($(window).width() - (hamOffset.left + ham.outerWidth()));
+  let hamOffsetBottom = hamOffset.top + ham.outerHeight();
+  let headerNav       = $('nav#headerNav');
+
+  if (!headerNav.hasClass('toggled')) {
+    headerNav.appendTo('body');
+  }
+
+  headerNav.addClass('toggled shadow');
+
+  headerNav
+    .toggleClass('is-visible')
+    .removeClass('align-items-center d-lg-flex')
+    .css('top',hamOffsetBottom)
+    .css('right',hamOffsetRight);
 }
 
-function configPopover($) {
-    var pop = $('[data-toggle="popover"]');
-    pop.each(function () {
-        var $elem = $(this);
-        $elem.popover({
-            placement: 'bottom',
-            trigger: 'hover',
-            html: false,
-            container: $elem,
-            animation: false
-        });
-    });
+//MATCH HEIGHT
+function matchHeight(set) {
+  var maxHeight = 0;
+  // collect data from all
+  set.each(function () {
+    var thisHeight = jQuery(this).outerHeight();
+    if (thisHeight > maxHeight) {
+      maxHeight = thisHeight;
+    }
+  });
+  // set data to all by max
+  set.each(function () {
+    jQuery(this).outerHeight(maxHeight);
+  });
+}
+
+//DOWNLOADS MATCH HEIGHT
+function downloadsMatchHeight() {
+  matchHeight(jQuery('.downloads-item-wrapper .text h4'));
+  matchHeight(jQuery('.downloads-item-wrapper .text'));
+  matchHeight(jQuery('.downloads-item-wrapper .text-two'));
+  matchHeight(jQuery('.downloads-item-wrapper h3'));
+}
+
+//copy text to clipboard
+function copyToClipboard(string) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(string).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
