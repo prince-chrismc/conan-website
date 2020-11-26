b87e47c772fc4b33e6e874183d93cea52db40e8d
czoido <mrgalleta@gmail.com>
2020-11-26 12:54:21 +0100

    update members

---
+++ src/js/3_tribe-members-nov-2020.js
@@ -0,0 +1,16 @@
+var tribeMembers = [
+  {
+    name: 'John Doe',
+    image: 'photo_profil_-_somedsdss',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    linkedin: 'https://www.linkedin.com/xxxxx',
+    twitter: 'https://twitter.com/some89980807'
+  },
+  {
+      name: 'Jane Doe',
+      image: 'photo_profil_-_somedsdss',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      linkedin: 'https://www.linkedin.com/xxxxx',
+      twitter: 'https://twitter.com/some89980807'
+  },
+]
